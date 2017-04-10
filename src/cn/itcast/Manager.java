e2673a546dcc2db28bdd8d68a114e4e5ab83d13e
hf-zhaozhiming <729144668@qq.com>
2017-04-10 16:29:27 +0800

    练习程序，学习用！

---
+++ src/cn/itcast/Manager.java
@@ -0,0 +1,18 @@
+package cn.itcast;
+
+/**
+ * 管理员类
+ * 
+ * @author Administrator
+ * 
+ */
+public class Manager {
+
+	public Manager() {
+		username = "itcast";
+		password = "itcast";
+	}
+
+	public String username;
+	public String password;
+}
